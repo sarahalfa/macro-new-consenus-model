93affba2a96820e52dff8f738d620a71a1cab239
Sarah Alfathlee <alfathlees@hotmail.com>
2022-02-15 17:41:03 +0000

    R studio recognises line 12 equation as true however this isnt connected to the graph * work on this at another time as currenlty brain fried

--- inflation_rate_graph.R
+++ inflation_rate_graph.R
@@ -7,7 +7,7 @@ assign (matrixname, (round(cbind(y,pi,r), digits=3)))
 
 #Plots 
 plot(y, type="l", xlab="y (output)", ylab="r (real interest rate)")
+plot(pi^t, type="l", xlab="y (output)", ylab="pi (inflation target)")
 
 #on the IR graph, there are 3 Phillips Curve's * implement those for now
-
 pi^t==pi^t+a*(y^t-ye)
