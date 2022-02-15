f3738fc8d9806e1f572abc00c6624b50c14b9657
Sarah Alfathlee <alfathlees@hotmail.com>
2022-02-15 17:31:22 +0000

    phillips curve here displaying true in the console ** thus refer to this phillips curve syntax

--- inflation_rate_graph.R
+++ inflation_rate_graph.R
@@ -7,3 +7,7 @@ assign (matrixname, (round(cbind(y,pi,r), digits=3)))
 
 #Plots 
 plot(y, type="l", xlab="y (output)", ylab="r (real interest rate)")
+
+#on the IR graph, there are 3 Phillips Curve's * implement those for now
+
+pi^t==pi^t+a*(y^t-ye)
