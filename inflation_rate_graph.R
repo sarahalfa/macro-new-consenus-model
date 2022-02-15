f57e6d08202c7f6cde00ca471a28e24eb4955e42
Sarah Alfathlee <alfathlees@hotmail.com>
2022-02-15 17:16:25 +0000

    x and y coordinates plotted for inflation rate graph - main issue is how to incorporate the actual data to show up on the graph ??? ie adding in the 3 curves

--- inflation_rate_graph.R
+++ inflation_rate_graph.R
@@ -1,6 +1,9 @@
 
 #Inflation Rate Graph
 
+#Table for the vectors of endogenous variables
 matrixname<-past("Inflation Rate Graph")
 assign (matrixname, (round(cbind(y,pi,r), digits=3)))
 
+#Plots 
+plot(y, type="l", xlab="y (output)", ylab="r (real interest rate)")
