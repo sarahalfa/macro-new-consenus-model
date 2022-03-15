e1a91c503346f586f2936f9c5cbade756cb2d09e
Sarah Alfathlee <alfathlees@hotmail.com>
2022-03-15 21:46:20 +0000

    x

--- script.R
+++ script.R
@@ -14,40 +14,46 @@ gamma <- 2
 # pi^T=0.02 inflation target (uppercase T is thus target)
 T <- "target"
 pi <- "inflation rate"
+
+
+pi[T]<-0.02 #this works but unsure if the syntax is correct??
+
 pi^T=="inflation target"
 pi^T==0.02 #prints false which is a concern; needs to show in env
 
 e <- "equilibrium"
-ye <- 1 
-y[1] <- ye
+y[e] <- 1 
+y[1] <- y[e]
 pi[1] <- pi^T #may need to change this to pi[1] <- 0.02 in the long run
-r <- "real interest rate"
 r[1] <- A-ye/gamma
 
 #Defined t to hold 20 time periods, from 1-20; this works fine 
 
 t <- "time"
-t <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+t <- c(1:20)
 
 #3 curves
 
 #IS curve (shows the demand-side of the economy)
 
-y[t]<-A-(gamma*r[t-1])
+y[t]<-A-gamma*r[t-1]
+
 
 #Phillips curve (shows supply-side of the economy)
 
-pi[t]<-pi[t-1]+alpha*(y[t]-ye)
+pi[t]<-pi[t-1]+alpha*(y[t]-y[e])
 
 #Interest rate rule
 
 r[t]<- A-ye/gamma+alpha*beta*pi[t]-(pi^T)/gamma*(1+(alpha^2)*beta)
 
+r[t]<-r[1]+alpha*beta*pi[t]-pi[T]/gamma*(1+alpha^2*beta)
+
 #Plot 3 graphs; inflation rate; interest rate; output over the 20 periods 
 
 #Table showing data for all 3
 
-matrixname <- paste("table")
+matrixname<-paste("table")
 assign (matrixname,(round(cbind(y,pi,r), digits=3)))
         
 
@@ -72,7 +78,8 @@ for (t in 2:20){
   if (t < 5) {A<-1.09} else {A<-1.1}
   y[t]<-A-(gamma*r[t-1])
   pi[t]<-pi[t-1]+alpha*(y[t]-ye)
-  r[t]<- A-ye/gamma+alpha*beta*pi[t]-(pi^T)/gamma*(1+(alpha^2)*beta)
+  r[t]<-r[1]+alpha*beta*pi[t]-pi[T]/gamma*(1+alpha^2*beta)
+  
   
   
   
