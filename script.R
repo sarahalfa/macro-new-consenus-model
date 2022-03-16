d70e7e14cf95c35eda0476a51fb75e52e551eb58
Sarah Alfathlee <alfathlees@hotmail.com>
2022-03-16 19:01:01 +0000

    misc

--- script.R
+++ script.R
@@ -1,4 +1,6 @@
 
+#code v1 (2nd script diff in the graphical output)
+
 #NEW CONSENSUS 3-EQUATION MODEL
 
 #Data
@@ -16,33 +18,32 @@ r <- vector(length=20)
 
 #Initial values defined
 
-pi^T==0.02
+pipowerT==0.02
 ye <- 1 
 y[1]<- ye
-pi[1] <- pi^T==0.02 
+pi[1] <- pipowerT==0.02
 r[1] <- A-ye/gamma
 
 #Defined t to hold 20 time periods, from 1-20; this works fine 
 
-t <- "time"
 t <- c(1:20)
 
 #3 curves
 
 #IS curve (shows the demand-side of the economy)
 #equation works 
-y[t]<-A-gamma*r[t-1]
+y[t]<-(A)-(gamma*r[t-1])
 
 
 #Phillips curve (shows supply-side of the economy)
 #equation works 
-pi[t]<-pi[t-1]+alpha*(y[t]-ye)
+pi[t]<-(pi[t-1])+(alpha*(y[t]-ye))
 
 #Interest rate rule
 #equation seems to work
 # error of 'no. of items to replace not a multiple of replacement length?
 
-r[t]<-(A-ye/gamma)+alpha*beta*(pi[t])-(pi^T)/gamma*(1+(alpha^2)*beta)
+r[t]<-((A-ye)/(gamma))+(alpha*beta*(pi[t]))-(pipowerT)/gamma*(1+(alpha^2)*beta)
 
 #Plot 3 graphs; inflation rate; interest rate; output over the 20 periods 
 
@@ -66,20 +67,36 @@ plot(r, type="l", xlab="Time Period", ylab="Interest rate rule")
 #For loop implemented to display this 
 #Need to ensure that after t=5, the rest of the time periods reflect this permanent shock
 
-for (t in 2:20){
-  if (t < 5) {A<-1.09} else {A<-1.1}
+
+for (t in 1:20){
+  while (t <= 5) 
+  {A<-1.09} 
+  else 
+  {A<-1.1}
+  y[t]<-A-(gamma*r[t-1])
+}
+  
+
+
+for (t in 1:20){
+  while (t <= 5) 
+    {A<-1.09} 
+  else 
+    {A<-1.1}
   y[t]<-A-(gamma*r[t-1])
   pi[t]<-pi[t-1]+alpha*(y[t]-ye)
-  r[t]<-(A-ye/gamma)+alpha*beta*(pi[t])-(pi^T)/gamma*(1+(alpha^2)*beta)
+  r[t]<-(A-ye/gamma)+alpha*beta*(pi[t])-(pipowerT)/gamma*(1+(alpha^2)*beta)
   
 }
+
+
 
 #below is an experiment to see if i should have the line of code to plot the graph within the for loop
 #this however still isnt producing a change at t=5
 #one thing to note is that this does plot the graphs, even w/ multiple equations in one
 #will have to see if this is the most efficient way to have it placed
 
-for (t in 2:20){
+for (t in 1:20){
   if (t <= 5) {A<-1.09} else {A<-1.1}
   y[t]<-A-(gamma*r[t-1])
   plot(y, type="l", xlab="Time Period", ylab="IS Curve")
@@ -89,4 +106,3 @@ for (t in 2:20){
 
 
 
-warnings()
