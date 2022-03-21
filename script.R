a79244fc68fa95bf007601b9da8cbfcc6ed06b16
Sarah Alfathlee <alfathlees@hotmail.com>
2022-03-21 15:47:56 +0000

    misc

--- script.R
+++ script.R
@@ -1,32 +1,25 @@
-
 #NEW CONSENSUS 3-EQUATION MODEL
 
-rm(list=ls(all=T))
+rm(list=ls(all=TRUE))
 
-#Data
-#Parameter values of A, alpha, beta and gamma 
-
-t <- 20
-A <- 1.1
-alpha <- 1
-beta <- 1
-gamma <- 2
-
-#Vectors
-y <- vector(length=20)
-pi <- vector(length=20)
-r <- vector(length=20)
-
-#Initial values defined
-
-pipowerT <-0.02 #pi when run is identified first at 0.02 
-ye <- 1 
-y[1]<- ye
-pi[1] <- pipowerT 
-r[1] <- A-ye/gamma
-
+#data 
+#parameter values
+A<-1.1
+alpha<-1
+beta<-1
+alpha == beta
+gamma<-2
+ye<-1
+pipowerT<-0.02
+#vectors
+y<-vector(length=20)
+pi<-vector(length=20)
+r<-vector(length=20)
+#initial values
+y[1]<-ye
+pi[1]<-pipowerT
+r[1]<-A-ye/gamma
 #3 curves
-
 #IS curve (shows the demand-side of the economy)
 #y[t]<-(A)-(gamma*r[t-1])
 
@@ -36,33 +29,21 @@ r[1] <- A-ye/gamma
 #Interest rate rule
 #r[t]<-((A-ye)/(gamma))+(alpha*beta*(pi[t]))-(pipowerT)/gamma*(1+(alpha^2)*beta)
 
-#Plot 3 graphs; inflation rate; interest rate; output over the 20 periods 
-#For loop created to reflect the negative aggregate demand shock occurring at t=5
-
+#plot 3 graphs; inflation rate; interest rate; output over the 20 periods 
+#for loop to reflect the negative aggregate demand shock at t=5
+t<-20
 
 for(t in 2:20){
-  if (t <= 5){
-    A<-1.09
-  } 
-  else {
-    A<-1.1
-    }
+  if (t <- 5){A<-1.09}
+  else {A<-1.1}
   y[t]<-A-(gamma*r[t-1])
   pi[t]<-pi[t-1]+alpha*(y[t]-ye)
-  r[t]<-(A-ye/gamma)+alpha*beta*(pi[t])-(pipowerT)/gamma*(1+(alpha^2)*beta)
-  
+  r[t]<-(A-ye/gamma)+alpha*beta*(pi[t]-pipowerT)/gamma*(1+(alpha^2)*beta)
 }
-
-#Table showing data for all 3
-matrixname<-paste("table")
-assign (matrixname,(round(cbind(y,pi,r), digits=3)))
-
-#3 plots created for endogenous variables y, pi and r for each curve 
+#table showing data for all 3
+matrixname<-paste("Table Results")
+assign(matrixname, round(cbind(y,pi,r, digits=3)))
+#3 plots created for endogenous variables y, pi and r for each curve
 plot(y, type="l", xlab="Time Period", ylab="IS Curve")
-
-plot(pi, type="l", xlab="Time Period", ylab="Phillips curve") 
-
+plot(pi, type="l", xlab="Time Period", ylab="Phillips Curve")
 plot(r, type="l", xlab="Time Period", ylab="Interest rate rule")
-
-
-
