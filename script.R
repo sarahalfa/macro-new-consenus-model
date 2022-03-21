7ea111d3610321418a292f7c544e8631b2b8509e
Sarah Alfathlee <alfathlees@hotmail.com>
2022-03-21 17:56:32 +0000

    very frustrating - graphs look how i want but x axis isnt as desired

--- script.R
+++ script.R
@@ -8,13 +8,13 @@ beta<-1
 alpha == beta
 gamma<-2
 #vectors
-y<-vector(length=20)
-pi<-vector(length=20)
-r<-vector(length=20)
+#y<-vector(length=20)
+#pi<-vector(length=20)
+#r<-vector(length=20)
 
-#y<-rep(c(0),times=20)
-#pi<-rep(c(0),times=20)
-#r<-rep(c(0),times=20)
+y<-rep(c(0),times=200)
+pi<-rep(c(0),times=200)
+r<-rep(c(0),times=200)
 
 #initial values
 ye<-1
@@ -31,14 +31,21 @@ r[1]<-A-ye/gamma
 #r[t]<-((A-ye)/(gamma))+(alpha*beta*(pi[t]))-(pipowerT)/gamma*(1+(alpha^2)*beta)
 #plot 3 graphs; inflation rate; interest rate; output over the 20 periods 
 #for loop to reflect the negative aggregate demand shock at t=5
-t<-seq(2, 5, 0.1)
+t<-seq(1, 20, 0.1)
+
 for(i in t){
-  if (i <= 5){A <- 1.09}
-  else {A <- 1.1}
+  if (i <= 5){
+    A <- 1.09
+    }
+  else {
+    A <- 1.1
+    }
   y[i]<-A-(gamma*r[t-1])
   pi[i]<-pi[t-1]+alpha*(y[t]-ye)
   r[i]<-(A-ye/gamma)+alpha*beta*(pi[t]-pipowerT)/gamma*(1+(alpha^2)*beta)
 }
+
+
 #table showing data for all 3
 matrixname<-paste("Table Results")
 assign (matrixname,(round(cbind(y,pi,r), digits=3)))
@@ -48,5 +55,5 @@ plot(y, type="l", xlab="Time Period", ylab="IS Curve")
 plot(pi, type="l", xlab="Time Period", ylab="Phillips Curve")
 plot(r, type="l", xlab="Time Period", ylab="Interest rate rule")
 
-warnings()
+
 
