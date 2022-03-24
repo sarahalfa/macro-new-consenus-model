ae5c7069986fbcc4de50ccc2b2baa6eb785d54d4
Sarah Alfathlee <alfathlees@hotmail.com>
2022-03-24 19:20:14 +0000

    cleaning up

--- script.R
+++ script.R
@@ -16,12 +16,12 @@ pi<-vector(length=20)
 r<-vector(length=20)
 
 #initial values
-ye<-1
-pipowerT<-0.02
+ye<-1 #equilibrium output 
+pipowerT<-0.02 #inflation target 
 
-y[1]<-1
-pi[1]<-0.02
-r[1]<-0.05
+y[1]<-1 #output 
+pi[1]<-0.02 #inflation rate 
+r[1]<-0.05 #real interest rate 
 
 #3 curves
 #IS curve (shows the demand-side of the economy)
@@ -31,22 +31,21 @@ r[1]<-0.05
 #pi[t]<-(pi[t-1])+(alpha*(y[t]-ye))
 #pi[t]<-pi[t-1]+alpha*(y[t]-1)
 
-#Interest rate rule
+#Interest rate rule equation 
 #r[t]<-((A-ye)/(gamma))+(alpha*beta*(pi[t]))-(pipowerT)/gamma*(1+(alpha^2)*beta)
 #r[t]<-((A*1/2)-1/2)+(((pi[t]-pi[1]))/4)
 
 #plot 3 graphs; inflation rate; interest rate; output over the 20 periods 
 #for loop to reflect the negative aggregate demand shock at t=5
-
 for(t in 2:20){
   if(t<5){A<-1.1}else{A<-1.09}
   y[t]<-A-gamma*r[t-1]
   pi[t]<-pi[t-1]+alpha*(y[t]-1)
   r[t]<-((A*1/2)-1/2)+(((pi[t]-pi[1]))/4)
 }
-  
-  
-#table showing data for all 3
+
+
+#table showing data for all 3 endogenous variables 
 matrixname<-paste("Table Results")
 assign (matrixname,(round(cbind(y,pi,r), digits=3)))
 
@@ -54,6 +53,8 @@ assign (matrixname,(round(cbind(y,pi,r), digits=3)))
 plot(y, type="l", xlab="Time Period", ylab="IS Curve")
 plot(pi, type="l", xlab="Time Period", ylab="Phillips Curve")
 plot(r, type="l", xlab="Time Period", ylab="Interest rate rule")
+
+
 
 
 
